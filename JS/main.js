cd297ae1e1af9e700f801a8ff3b4b7a552cf1c18
Isaac Hardy <isaachardyDEV@gmail.com>
2015-10-05 15:23:36 -0400

    Added function to display the proper time with zeros - Added interval funciton to run clock function every one second - Added lineheight to CSS

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,28 @@
 (function (){
 
 
+  // Time function
+  function clock () {
 
+    // Create Date with hour/minute/second variables
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
 
-// Create Date with hour/minute/second variables
-var today = new Date();
-var hours = today.getHours();
-var minutes = today.getMinutes();
-var seconds = today.getSeconds();
+    // Jquery Assignment to text ID
+    var hourZero = (hours < 10) 
+      ? $('#hour').text('0' + hours) : $('#hour').text(hours);
+    var minZero = (minutes < 10) 
+      ? $('#minute').text(':0' + minutes) : $('#minute').text(':' + minutes);
+    var secZero = (seconds < 10) 
+      ? $('#second').text(':0' + seconds) : $('#second').text(':' + seconds);
 
-// Jquery Assignment to text ID
-$('#hour').text(hours);
-$('#minute').text(':' + minutes);
-$('#second').text(':' + seconds);
+  };
 
+  clock();
 
+  setInterval(clock,1000);
 
 
 
